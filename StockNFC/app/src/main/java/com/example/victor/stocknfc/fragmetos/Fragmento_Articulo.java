2f7eb6d7a56bffaeb3c095974db26b329fee9c5d
vdseoane <victordiazseoane@gmail.com>
2018-04-22 11:45:54 +0200

    Eliminacion codigo sobrante
    Se elimina codigo sobrante de imagen

--- StockNFC/app/src/main/java/com/example/victor/stocknfc/fragmetos/Fragmento_Articulo.java
+++ StockNFC/app/src/main/java/com/example/victor/stocknfc/fragmetos/Fragmento_Articulo.java
@@ -78,18 +78,9 @@ public class Fragmento_Articulo extends android.support.v4.app.Fragment {
     byte[] imageInByte;
 
     //Datos obtencion imagen
-    private static final int ACTIVITY_SELECT_IMAGE = 1020, ACTIVITY_SELECT_FROM_CAMERA = 1040, ACTIVITY_SHARE =1030, MY_CAMERA_PERMISSION_CODE = 1050;
-    private static String APP_DIRECTORY = "MyPictures/";
-    private static String MEDIA_DIRECTORY = APP_DIRECTORY + "PictureApp";
-    private AlertDialog photoDialog;
-    private PhotoUtils photoUtils;
-    private Uri mImageUri;
-    private String myPath;
-
-
+    private static final int ACTIVITY_SELECT_IMAGE = 1020, ACTIVITY_SELECT_FROM_CAMERA = 1040, MY_CAMERA_PERMISSION_CODE = 1050;
 
     public Fragmento_Articulo() {
-        // Required empty public constructor
     }
 
     @Override
@@ -110,34 +101,31 @@ public class Fragmento_Articulo extends android.support.v4.app.Fragment {
         crearOnClicks();
 
         fechaArticulo.setText(fechaArt);
+    }
 
-        // Procesado de la obtención de la imagen
-/*        Intent intent = getActivity().getIntent();
-        String action = intent.getAction();
-        String type = intent.getType();
-        if (Intent.ACTION_SEND.equals(action) && type != null) {
-            if ("text/plain".equals(type)) {
-                //fromShare = true;
-            } else if (type.startsWith("image/")) {
-                //fromShare = true;
-                mImageUri = (Uri) intent
-                        .getParcelableExtra(Intent.EXTRA_STREAM);
-                getImage(mImageUri);
-            }
-        }*/
+    private void obtenerTextViews() {
+        nombreArticulo = getView().findViewById(R.id.nombreArticulo);
+        stockArticulo = getView().findViewById(R.id.stockArticulo);
+        alertaArticulo = getView().findViewById(R.id.alertaArticulo);
+        precioArticulo = getView().findViewById(R.id.precioArticulo);
+        proveedorArticulo = getView().findViewById(R.id.proveedorArticulo);
+        imgArticulo = getView().findViewById(R.id.imgArticulo);
+        fechaArticulo = getView().findViewById(R.id.fechaArticulo);
+        btnGuardarArticulo = getView().findViewById(R.id.btnGuardarArticulo);
     }
 
     private void crearOnClicks() {
         imgArticulo.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
-                    seleccionarImagenDialogo();
-        }});
+            public void onClick(View v) {
+                seleccionarImagenDialogo();
+            }
+        });
 
         btnGuardarArticulo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
-                    if(comprobarArticuloCorrecto()){
+                    if (comprobarArticuloCorrecto()) {
                         obtenerDatosGuardar();
                         guardarArticulo();
                     }
@@ -151,9 +139,9 @@ public class Fragmento_Articulo extends android.support.v4.app.Fragment {
     private void seleccionarImagenDialogo() {
         try {
             PackageManager pm = getActivity().getPackageManager();
-            int hasPerm = pm.checkPermission(Manifest.permission.CAMERA,  getActivity().getPackageName());
+            int hasPerm = pm.checkPermission(Manifest.permission.CAMERA, getActivity().getPackageName());
             if (hasPerm == PackageManager.PERMISSION_GRANTED) {
-                final CharSequence[] options = {"Take Photo", "Choose From Gallery","Cancel"};
+                final CharSequence[] options = {"Take Photo", "Choose From Gallery", "Cancel"};
                 android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(getActivity());
                 builder.setTitle("Select Option");
                 builder.setItems(options, new DialogInterface.OnClickListener() {
@@ -173,7 +161,7 @@ public class Fragmento_Articulo extends android.support.v4.app.Fragment {
                     }
                 });
                 builder.show();
-            } else{
+            } else {
                 if (getContext().checkSelfPermission(
                         Manifest.permission.CAMERA)
                         != PackageManager.PERMISSION_GRANTED) {
@@ -200,69 +188,14 @@ public class Fragmento_Articulo extends android.support.v4.app.Fragment {
         }
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        if (mImageUri != null)
-            outState.putString("Uri", mImageUri.toString());
-    }
-
-    @Override
-    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
-        super.onViewStateRestored(savedInstanceState);
-
-/*        if (savedInstanceState.containsKey("Uri")) {
-            mImageUri = Uri.parse(savedInstanceState.getString("Uri"));
-        }*/
-    }
-
-/*    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ACTIVITY_SELECT_IMAGE && resultCode == getActivity().RESULT_OK) {
-            mImageUri = data.getData();
-            getImage(mImageUri);
-        } else if (requestCode == ACTIVITY_SELECT_FROM_CAMERA
-                && resultCode == getActivity().RESULT_OK) {
-            getImage(mImageUri);
-        }
-    }*/
-
-    public void getImage(Uri uri) {
-        Bitmap bounds = photoUtils.getImage(uri);
-        if (bounds != null) {
-            imgArticulo.setImageBitmap(bounds);
-        } else {
-            //showErrorToast();
-
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     private void guardarArticulo() {
         bdArticulo = new ArticuloDB(getContext());
 
         long inserccion = bdArticulo.insertarArticulo(bdArticulo.getWritableDatabase(), articulo);
 
-        if(inserccion != -1){
+        if (inserccion != -1) {
             Toast.makeText(getContext(), "Articulo añadido correctamente", Toast.LENGTH_LONG).show();
-        }else{
+        } else {
             Dialogo dialogo = new Dialogo(getContext(), "Ha habido un error al insertar el artículo");
             dialogo.getDialogo().show();
         }
@@ -271,30 +204,17 @@ public class Fragmento_Articulo extends android.support.v4.app.Fragment {
     private void obtenerDatosGuardar() throws ParseException {
         articulo.setNombre(nombreArticulo.getText().toString());
         articulo.setStock(Integer.parseInt(stockArticulo.getText().toString()));
-//        alerta = Integer.parseInt(alertaArticulo.getText().toString());
         articulo.setFechaCreacion(fechaArticulo.getText().toString());
-//        precio = Float.parseFloat(precioArticulo.getText().toString());
         articulo.setProveedor(proveedorArticulo.getText().toString());
 
-        //Obtenemos la imagen
-//        BitmapDrawable bitmapDrawable = ((BitmapDrawable) imgArticulo.getDrawable());
-//        Bitmap bitmap = bitmapDrawable .getBitmap();
-//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-//        imageInByte = stream.toByteArray();
     }
 
-/*    private void abrirGaleria() {
-        Intent abrirGaleria = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(abrirGaleria, OBTENER_IMAGEN);
-    }
-*/
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        if(requestCode == ACTIVITY_SELECT_IMAGE && resultCode == getActivity().RESULT_OK){
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ACTIVITY_SELECT_IMAGE && resultCode == getActivity().RESULT_OK) {
             imgUri = data.getData();
             imgArticulo.setBackground(null);
             imgArticulo.setImageURI(imgUri);
-        }else if (requestCode == ACTIVITY_SELECT_FROM_CAMERA
+        } else if (requestCode == ACTIVITY_SELECT_FROM_CAMERA
                 && resultCode == getActivity().RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imgArticulo.setBackground(null);
@@ -302,45 +222,34 @@ public class Fragmento_Articulo extends android.support.v4.app.Fragment {
         }
     }
 
-    private void obtenerTextViews() {
-        nombreArticulo = getView().findViewById(R.id.nombreArticulo);
-        stockArticulo = getView().findViewById(R.id.stockArticulo);
-        alertaArticulo = getView().findViewById(R.id.alertaArticulo);
-        precioArticulo = getView().findViewById(R.id.precioArticulo);
-        proveedorArticulo = getView().findViewById(R.id.proveedorArticulo);
-        imgArticulo = getView().findViewById(R.id.imgArticulo);
-        fechaArticulo = getView().findViewById(R.id.fechaArticulo);
-        btnGuardarArticulo = getView().findViewById(R.id.btnGuardarArticulo);
-    }
-
     private boolean comprobarArticuloCorrecto() throws ParseException {
-        if(validaciones.textoNoNulo(nombreArticulo.getText().toString())){
+        if (validaciones.textoNoNulo(nombreArticulo.getText().toString())) {
             dialogo = new Dialogo(getContext(), getContext().getResources().getString(R.string.msnNombreNulo));
             dialogo.getBuilder().create().show();
             return false;
-        }else if(validaciones.textoNoNulo(stockArticulo.getText().toString())){
+        } else if (validaciones.textoNoNulo(stockArticulo.getText().toString())) {
             dialogo = new Dialogo(getContext(), getContext().getResources().getString(R.string.msnStockNulo));
             dialogo.getBuilder().create().show();
             return false;
-        }else if(validaciones.numeroNegativo(Integer.parseInt(stockArticulo.getText().toString()))){
+        } else if (validaciones.numeroNegativo(Integer.parseInt(stockArticulo.getText().toString()))) {
             dialogo = new Dialogo(getContext(), getContext().getResources().getString(R.string.msnStockNegativo));
             dialogo.getBuilder().create().show();
-            return  false;
-        }else {
+            return false;
+        } else {
             //Alerta
             if (validaciones.textoNoNulo(alertaArticulo.getText().toString())) {
                 articulo.setAlertaStock(-1);
-            }else articulo.setAlertaStock(Integer.parseInt(alertaArticulo.getText().toString()));
+            } else articulo.setAlertaStock(Integer.parseInt(alertaArticulo.getText().toString()));
             //Precio
-            if (validaciones.textoNoNulo(precioArticulo.getText().toString())){
+            if (validaciones.textoNoNulo(precioArticulo.getText().toString())) {
                 articulo.setPrecio(0);
             } else articulo.setPrecio(Float.parseFloat(precioArticulo.getText().toString()));
 
             //Imagen
             //Obtenemos la imagen
             BitmapDrawable bitmapDrawable = ((BitmapDrawable) imgArticulo.getDrawable());
-            if(!validaciones.esNulo(bitmapDrawable)){
-                Bitmap bitmap = bitmapDrawable .getBitmap();
+            if (!validaciones.esNulo(bitmapDrawable)) {
+                Bitmap bitmap = bitmapDrawable.getBitmap();
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
                 bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
                 articulo.setImagenArticulo(stream.toByteArray());
